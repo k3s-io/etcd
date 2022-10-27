92862d124b9e0e504c9627a7a7fca710f87d62ff
galal-hussein <hussein.galal.ahmed.11@gmail.com>
2022-10-27 19:57:27 +0200

    Adding tag.sh script to add new version tags

---
+++ tag.sh
@@ -0,0 +1,26 @@
+#!/bin/bash
+set -e
+
+if [ -z "$1" ]; then
+    echo usage: $0 TAG
+    exit 1
+fi
+
+if [ -n "$(git tag -l $1)" ]; then
+    echo $1 tag exists run
+    echo "    " git tag -d $1
+    exit 1
+fi
+
+git tag $1
+TAGS="refs/tags/$1"
+
+for i in */; do
+  if [ -f $i/go.mod ]; then
+	  git tag -d $i$1 2>/dev/null || true
+    git tag $i$1
+    TAGS="$TAGS refs/tags/$i$1"
+  fi
+done
+
+echo git push '$REMOTE' "$TAGS"
